610765243eee86b5d0d06340ae568bb0f9a68081
mainamirh <mainamirh@gmail.com>
2022-11-15 19:08:12 +0330

    final 1

--- pages/api/auth/request-message.js
+++ pages/api/auth/request-message.js
@@ -3,7 +3,7 @@ import Moralis from "moralis";
 const config = {
   domain: process.env.APP_DOMAIN,
   statement: "Please sign this message to confirm your identity.",
-  uri: process.env.NEXTAUTH_URL,
+  // uri: process.env.NEXTAUTH_URL,
   timeout: 60,
 };
 
